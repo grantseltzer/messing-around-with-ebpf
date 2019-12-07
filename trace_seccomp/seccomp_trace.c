6514f69814dd51a4d03e30127b6c6f29d070b9b0
grantseltzer <grantseltzer@gmail.com>
2019-12-06 23:03:00 -0500

    ebpf now reads in filter
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

--- trace_seccomp/seccomp_trace.c
+++ trace_seccomp/seccomp_trace.c
@@ -31,35 +31,48 @@ static __always_inline int init_context(context_t *context) {
     return 0;
 }
 
-// Kprobe functions
+// function that's actually called on process calling seccomp(2)
 int trace_and_print_seccomp_calls(struct pt_regs *ctx) {
     
     context_t context = {};
     init_context(&context);
 
+    // Read in seccomp(2) arguments manually from registers (can't use bcc bindings because of bug)
     struct pt_regs * ctx2 = (struct pt_regs *)ctx->di;
     bpf_probe_read(&context.operation, sizeof(context.operation), &ctx2->di);
     bpf_probe_read(&context.flags, sizeof(context.flags), &ctx2->si);
     bpf_probe_read(&context.args, sizeof(struct sock_fprog*), &ctx2->dx);
 
-    if (context.flags == 1 && context.operation == 1 && context.args != NULL) {
-        // args is a pointer to a sock_fprog struct
-        
+    // If call is installing filters
+    if (context.operation == 1 && context.args != NULL) {
 
-        struct sock_fprog *bpfprogptr = (struct sock_fprog*)context.args;
+        // cast context.args from void* to a pointer to a sock_fprog struct
+        // and then read the full structure into memory so BPF knows about it
+        struct sock_fprog *bpfprog_ptr = (struct sock_fprog*)context.args;
         struct sock_fprog bpfprog = {}; 
-        bpf_probe_read(&bpfprog, sizeof(bpfprog), bpfprogptr);
+        bpf_probe_read(&bpfprog, sizeof(bpfprog), bpfprog_ptr);
         bpf_probe_read(&bpfprog.len, sizeof(bpfprog.len), &bpfprog.len);
         bpf_probe_read(&bpfprog.filter, sizeof(bpfprog.filter), &bpfprog.filter);
-
-        bpf_trace_printk("%x\n", bpfprog);
+       
         bpf_trace_printk("%d\n", bpfprog.len);
         bpf_trace_printk("%x\n", bpfprog.filter);
 
+        // Read in a single bpf filter
+        struct sock_filter first_filter = {}; 
+        bpf_probe_read(&first_filter, sizeof(first_filter), bpfprog.filter);
+        bpf_probe_read(&first_filter.code, sizeof(first_filter.code), &first_filter.code);
+        bpf_trace_printk("code of first instruction: %d\n", first_filter.code);
 
+    
+        // TODO: read through filters, determine the syscall numbers, put them in the context, then submit the context:
         // seccomps.perf_submit(ctx, &context, sizeof(context));
     }
 
     return 0;
 }
 
+
+/*
+        for (i = 0; i < bpfprog.len; i++) {
+
+*/
