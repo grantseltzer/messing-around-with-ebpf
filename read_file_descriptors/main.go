d1b1b303f473b92d7a74fecce9f0e77b364f0b61
grantseltzer <grantseltzer@gmail.com>
2020-08-04 21:18:37 -0400

    code for pulling data out of kernel
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

--- read_file_descriptors/main.go
+++ read_file_descriptors/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
+	"errors"
 	"log"
 	"os"
 	"os/signal"
@@ -15,7 +18,10 @@ import (
 const eBPF_Program = `
 #include <uapi/linux/ptrace.h>
 
+BPF_PERF_OUTPUT(events);
+
 typedef struct mmap_args {
+	u32 pid;
 	void* addr;
 	size_t length;
 	int prot;
@@ -27,8 +33,8 @@ typedef struct mmap_args {
 
 int trace_mmap(struct pt_regs *ctx) {
 
-	u32 pid = (u32)bpf_get_current_pid_tgid();
 	args_t args = {};
+	args.pid = (u32)bpf_get_current_pid_tgid();
 
 	// In kernel 4.17+ the actual context is stored by reference in di register
 	struct pt_regs * actualCtx = (struct pt_regs *)ctx->di;
@@ -39,10 +45,38 @@ int trace_mmap(struct pt_regs *ctx) {
 	bpf_probe_read(&args.fd, sizeof(args.fd), &actualCtx->r8);
 	bpf_probe_read(&args.offset, sizeof(args.offset), &actualCtx->r9);
 
-	bpf_trace_printk("(%d) %d\n", pid, args.fd);
+	events.perf_submit(ctx, &args, sizeof(args));
+
 	return 0;
 }
 `
+
+type mmap_args struct {
+	processID uint32
+	addr      uint64
+	length    uint64
+	prot      uint32
+	flags     uint32
+	fd        uint32
+	offset    uint64
+}
+
+func (m *mmap_args) unmarshalBinaryData(data []byte) error {
+
+	if len(data) != 40 {
+		return errors.New("incorrect number of bytes in binary data for decoding")
+	}
+
+	data = bytes.Trim(data, "\x00")
+	m.processID = binary.LittleEndian.Uint32(data[0:4])
+	m.addr = binary.LittleEndian.Uint64(data[4:12])
+	m.length = binary.LittleEndian.Uint64(data[12:20])
+	m.prot = binary.LittleEndian.Uint32(data[20:24])
+	m.flags = binary.LittleEndian.Uint32(data[24:28])
+	m.fd = binary.LittleEndian.Uint32(data[28:32])
+	m.offset = binary.LittleEndian.Uint64(data[32:40])
+	return nil
+}
 
 func main() {
 
@@ -60,26 +94,94 @@ func main() {
 		log.Fatal(err)
 	}
 
+	table := bcc.NewTable(bpfModule.TableId("events"), bpfModule)
+	channel := make(chan []byte)
+	perfMap, err := bcc.InitPerfMap(table, channel, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		for {
+			value := <-channel
+			mmapInfo := mmap_args{}
+			mmapInfo.unmarshalBinaryData(value)
+		}
+	}()
+
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 
 	<-c
 }
 
-func printMemoryProtectionFlag(flag uint32) string {
+func printMemoryProtectionFlag(prot uint32) string {
 	var protectionFlags []string
-	if flag == 0x0 {
+	if prot == 0x0 {
 		protectionFlags = append(protectionFlags, "PROT_NONE")
 	}
-	if flag&0x01 == 0x01 {
+	if prot&0x01 == 0x01 {
 		protectionFlags = append(protectionFlags, "PROT_READ")
 	}
-	if flag&0x02 == 0x02 {
+	if prot&0x02 == 0x02 {
 		protectionFlags = append(protectionFlags, "PROT_WRITE")
 	}
-	if flag&0x04 == 0x04 {
+	if prot&0x04 == 0x04 {
 		protectionFlags = append(protectionFlags, "PROT_EXEC")
 	}
 
 	return strings.Join(protectionFlags, "|")
+}
+
+func printMemoryVisibilityFlag(vis uint32) string {
+
+	var visibilityFlags []string
+
+	if vis&0x01 == 0x01 {
+		visibilityFlags = []string{"MAP_SHARED"}
+	}
+	if vis&0x02 == 0x02 {
+		visibilityFlags = []string{"MAP_PRIVATE"}
+	}
+	if vis&0x02 == 0x03 {
+		visibilityFlags = []string{"MAP_SHARED_VALIDATE"}
+	}
+	if vis&0x0f == 0x10 {
+		visibilityFlags = []string{"MAP_ANONYMOUS"}
+	}
+	if vis&0x0f == 0x100 {
+		visibilityFlags = []string{"MAP_FIXED"}
+	}
+	if vis&0x0f == 0x40 {
+		visibilityFlags = []string{"MAP_32BIT"}
+	}
+	if vis&0x0f == 0x200000 {
+		visibilityFlags = []string{"MAP_FIXED_NOREPLACE"}
+	}
+	if vis&0x0f == 0x01000 {
+		visibilityFlags = []string{"MAP_GROWSDOWN"}
+	}
+	if vis&0x0f == 0x100000 {
+		visibilityFlags = []string{"MAP_HUGETLB"}
+	}
+	if vis&0x0f == 0x08000 {
+		visibilityFlags = []string{"MAP_LOCKED"}
+	}
+	if vis&0x0f == 0x40000 {
+		visibilityFlags = []string{"MAP_NONBLOCK"}
+	}
+	if vis&0x0f == 0x20000 {
+		visibilityFlags = []string{"MAP_POPULATE"}
+	}
+	if vis&0x0f == 0x10000 {
+		visibilityFlags = []string{"MAP_NORESERVE"}
+	}
+	if vis&0x0f == 0x80000 {
+		visibilityFlags = []string{"MAP_STACK"}
+	}
+	if vis&0x0f == 0x4000000 {
+		visibilityFlags = []string{"MAP_UNINITIALIZED"}
+	}
+
+	return strings.Join(visibilityFlags, "|")
 }
